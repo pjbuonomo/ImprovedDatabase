4d44f09a77f84e79c96f39a7dba771f10600b0e1
pjbuonomo <81430048+pjbuonomo@users.noreply.github.com>
2024-01-16 09:21:12 -0500

    Update Swiss_Clean.r

--- Old Clean/Swiss_Clean.r
+++ Old Clean/Swiss_Clean.r
@@ -1,3 +1,4 @@
+
 library(dplyr)
 library(DBI)
 library(RODBC)
@@ -8,33 +9,27 @@ db_conn <- odbcConnect("ILS")
 
 #################################################################################################################
 
-table <- read_excel("S:/Touchstone/Catrader/Boston/Database/CatBond/Shiny/Weekly_Pricing/ILS pricing data 20231229.xlsx", col_names = TRUE, skip = 12) %>%
-  na.omit() %>%
-  data.frame()
+table <- read_excel("S:/Touchstone/Catrader/Boston/Database/CatBond/Shiny/Weekly_Pricing/ILS pricing data 20231229.xlsx", col_names=T, skip=12) %>%
+		na.omit() %>%
+		data.frame()
 
-ColNames <- sqlColumns(db_conn, "SwissRe") %>%
-  select('COLUMN_NAME')
+ColNames <- sqlColumns(db_conn, "SwissRe") %>% 
+			select('COLUMN_NAME')
 
 result <- table %>%
-  select(-3, -4, -6, -8, -11, -12)
+		select(-3, -4, -6, -8, -11, -12)
 
 colnames(result) <- ColNames$COLUMN_NAME
 
 for (i in 1:nrow(result)) {
-  if (nchar(result$CUSIP[i]) > 9) {
-    result$CUSIP[i] <- substr(result$CUSIP[i], 3, 11)
-  }
+	if (nchar(result$CUSIP[i]) > 9) {
+		result$CUSIP[i] <- substr(result$CUSIP[i], 3, 11)
+	}
 }
 
-result$EL <- result$EL %>%
-  parse_number() %>%
-  as.integer()
-
-result$Coupon <- result$Coupon %>%
-  parse_number() %>%
-  as.integer()
-
-sqlSave(db_conn, result, tablename = "SwissRe", rownames = FALSE, append = TRUE, fast = FALSE, verbose = TRUE)
+result$EL <- result$EL %>% parse_number() %>% as.integer()
+result$Coupon <- result$Coupon %>% parse_number() %>% as.integer()
+sqlSave(db_conn, result, tablename="SwissRe", rownames=F, append=T, fast=F, verbose = TRUE)
 
 ##################################################################################################################################
 
