06447b893017feb30dd176d8550dfdb96f260ce8
pjbuonomo <81430048+pjbuonomo@users.noreply.github.com>
2024-01-16 09:21:25 -0500

    Update GCS_Clean.r

--- Old Clean/GCS_Clean.r
+++ Old Clean/GCS_Clean.r
@@ -1,3 +1,4 @@
+
 library(dplyr)
 library(DBI)
 library(RODBC)
@@ -6,27 +7,30 @@ library(readxl)
 
 db_conn <- odbcConnect("ILS")
 
-# Read data from Excel file
-table <- read_excel("S:/Touchstone/Catrader/Boston/Database/CatBond/Shiny/GCS_Weekly/GCS20231229.xlsx", sheet = 2, col_names = FALSE, skip = 6) %>%
-  data.frame()
+#################################################################################################################
 
-# Add date to the front of the table (Needs to be manually edited)
-Date <- rep(as.Date("2023-12-29"), times = nrow(table))
-table <- cbind(Date, table)
+table <- read_excel("S:/Touchstone/Catrader/Boston/Database/CatBond/Shiny/GCS_Weekly/GCS20231229.xlsx", sheet = 2, col_names=F, skip=6) %>%
+		data.frame()
 
-# Get column names from the database and set them as column names for the table
-ColNames <- sqlColumns(db_conn, "GCS") %>%
-  select('COLUMN_NAME')
+#Add date to front of table (Needs to be manually edited)
+Date <- rep(as.Date("2023-12-29"), times=nrow(table))
+table <-cbind(Date, table)
+
+ColNames <- sqlColumns(db_conn, "GCS") %>% 
+			select('COLUMN_NAME')
+
 colnames(table) <- ColNames$COLUMN_NAME
 
-# Limit BidSpread and OfferSpread to 2 decimal places and convert any N/A values to 0
+#Limit BidSpread and OfferSpread to 2 decimal places and convert any N/A values to 0
 table$BidSpread[is.na(table$BidSpread)] <- 0
 table$OfferSpread[is.na(table$OfferSpread)] <- 0
 table$BidSpread <- formatC(c(table$BidSpread), digits = 2, format = 'f')
 table$OfferSpread <- formatC(c(table$OfferSpread), digits = 2, format = 'f')
 
-# Save the table to the database
-sqlSave(db_conn, table, tablename = "GCS", rownames = FALSE, append = TRUE, fast = FALSE)
+sqlSave(db_conn, table, tablename="GCS", rownames=F, append=T, fast=F)
 
-# Close the database connection
+##################################################################################################################################
+
 odbcClose(db_conn)
+
+##################################################################################################################################
