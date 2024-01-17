61b50b0bf8954fb9f519b02d99a78ed9cdf68ac7
pjbuonomo <81430048+pjbuonomo@users.noreply.github.com>
2024-01-17 13:23:45 -0500

    Update PythonOutlook.r

--- Outlook Stuff/PythonOutlook.r
+++ Outlook Stuff/PythonOutlook.r
@@ -1,32 +1,37 @@
 library(reticulate)
 
 # Use reticulate to run Python code
-py_run_string('
+python_code <- "
 import win32com.client
 import os
 
 # Create an Outlook application object
-Outlook = win32com.client.Dispatch("Outlook.Application")
-namespace = Outlook.GetNamespace("MAPI")
+Outlook = win32com.client.Dispatch('Outlook.Application')
+namespace = Outlook.GetNamespace('MAPI')
 
 # Access the Inbox and then the specific subfolder
 inbox = namespace.GetDefaultFolder(6)  # 6 refers to the inbox
-bhCatBondFolder = inbox.Folders["BH Cat Bond"]
+bhCatBondFolder = inbox.Folders['BH Cat Bond']
 
 # Create a directory to store the text files
-outputDir = "S:/Touchstone/Catrader/Boston/Database/UnreadEmails"
+outputDir = 'S:/Touchstone/Catrader/Boston/Database/UnreadEmails'
 os.makedirs(outputDir, exist_ok=True)
 
 # Loop to process unread messages
 for message in bhCatBondFolder.Items:
     if message.UnRead:
+        # Prepare the filename, removing problematic characters
+        subject = message.Subject.replace(':', '').replace('/', '').replace('\\\\', '')
+        filename = f'{outputDir}/{subject}.txt'
         # Save the email as a text file
-        filename = f"{outputDir}/{message.Subject.replace(':', '').replace('/', '')}.txt"
         message.SaveAs(filename, 0)  # 0 is the olTXT format
         # Mark the message as read (optional)
         # message.UnRead = False
         # message.Save()
-')
+"
+
+# Run the Python code
+py_run_string(python_code)
 
 # Process the saved text files in R
 emailFiles <- list.files(path = "S:/Touchstone/Catrader/Boston/Database/UnreadEmails", pattern = "*.txt", full.names = TRUE)
